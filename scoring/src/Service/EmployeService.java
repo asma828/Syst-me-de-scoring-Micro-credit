d5a5920d6dbee3ad1e6e82993a97d9991c5cf5f2
asma828 <asmalachhab66@gmail.com>
2025-10-04 23:25:53 +0100

    create employe service

--- scoring/src/Service/EmployeService.java
+++ scoring/src/Service/EmployeService.java
@@ -1,4 +1,29 @@
 package Service;
 
+import DAO.EmployeDAO;
+import Model.Employe;
+
 public class EmployeService {
+    private EmployeDAO employeDAO = new EmployeDAO();
+    private ScoringService scoringService = new ScoringService();
+
+    public int ajouterEmploye(Employe employe) {
+        scoringService.calculerScore(employe);
+        return employeDAO.create(employe);
+    }
+
+    public boolean deleteEmploye(int id) {
+        return employeDAO.delete(id);
+    }
+
+    public boolean modifierEmploye(Employe employe) {
+        scoringService.calculerScore(employe);
+        return employeDAO.update(employe);
+    }
+
+    public Employe getEmployeById(int id) {
+        return employeDAO.findById(id);
+    }
+
+
 }
