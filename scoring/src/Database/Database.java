d1e051ea72b7efe56a9593aae3b3d78c9fe92ed9
asma828 <asmalachhab66@gmail.com>
2025-10-04 23:13:43 +0100

    create la connextion avec la base donneé

--- scoring/src/Database/Database.java
+++ scoring/src/Database/Database.java
@@ -1,4 +1,31 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+    private static Database instance;
+    private final String URL = "jdbc:mysql://localhost:3306/scoringsysteme";
+    private final String USER = "root";
+    private final String PASSWORD = "";
+
+    private Database() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver not found", e);
+        }
+    }
+
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
