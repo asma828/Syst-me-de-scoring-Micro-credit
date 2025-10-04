c8788b7b3c66d2df31bf4992d002440738ce3cd2
asma828 <asmalachhab66@gmail.com>
2025-10-04 23:26:20 +0100

    create professionnel service

--- scoring/src/Service/ProfessionnelService.java
+++ scoring/src/Service/ProfessionnelService.java
@@ -1,4 +1,29 @@
 package Service;
 
+import DAO.ProfessionnelDAO;
+import Model.Professionnel;
+
 public class ProfessionnelService {
+    private ProfessionnelDAO dao = new ProfessionnelDAO();
+    private ScoringService scoringService = new ScoringService();
+
+
+    public int ajouterProfessionnel(Professionnel p) {
+        scoringService.calculerScore(p);
+        return dao.create(p);
+    }
+
+    public boolean modifierProfessionnel(Professionnel p) {
+        scoringService.calculerScore(p);
+
+        return dao.update(p);
+    }
+
+    public Professionnel getProfessionnelById(int id) {
+        return dao.findById(id);
+    }
+
+    public boolean supprimerProfessionnel(int id) {
+        return dao.delete(id);
+    }
 }
