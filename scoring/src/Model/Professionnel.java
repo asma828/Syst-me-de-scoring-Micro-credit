f5577c64350388fca34bf6be71582b2973fd40e8
asma828 <asmalachhab66@gmail.com>
2025-10-04 23:16:31 +0100

    create professionnal model

--- scoring/src/Model/Professionnel.java
+++ scoring/src/Model/Professionnel.java
@@ -1,4 +1,56 @@
 package Model;
 
-public class Professionnel {
+
+import enums.Secteuractivite;
+import enums.Situation_Familiale;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Professionnel extends Personne {
+    private double revenu;
+    private String immatriculationfiscale;
+    private Secteuractivite secteuractivite;
+    private String activité;
+
+    public Professionnel(int id, String nom, String prenom, LocalDate datedenaissance, String ville, int nombreEnfants, boolean investissement, boolean placement, Situation_Familiale Situation_Familiale, int score, double revenu, String immatriculationfiscale, Secteuractivite secteuractivite, String activité) {
+        super(id, nom, prenom, datedenaissance, ville, nombreEnfants, investissement, placement, Situation_Familiale, score);
+        this.id = id;
+        this.revenu = revenu;
+        this.immatriculationfiscale = immatriculationfiscale;
+        this.secteuractivite = secteuractivite;
+        this.activité = activité;
+    }
+
+    public double getRevenu() {
+        return revenu;
+    }
+
+    public String getImmatriculationfiscale() {
+        return immatriculationfiscale;
+    }
+
+    public Secteuractivite getSecteuractivite() {
+        return secteuractivite;
+    }
+
+    public String getActivité() {
+        return activité;
+    }
+
+    public void setRevenu(double revenu) {
+        this.revenu = revenu;
+    }
+
+    public void setImmatriculationfiscale(String immatriculationfiscale) {
+        this.immatriculationfiscale = immatriculationfiscale;
+    }
+
+    public void setSecteuractivite(Secteuractivite secteuractivite) {
+        this.secteuractivite = secteuractivite;
+    }
+
+    public void setActivité(String activité) {
+        this.activité = activité;
+    }
 }
